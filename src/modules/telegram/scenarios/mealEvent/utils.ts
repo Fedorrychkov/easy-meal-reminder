34a77903d8aac21f23e091e45fa34b2213deea2a
Fedor Rychkov <fedorrychkov@yandex.ru>
2024-01-21 12:54:27 +0300

    fix: add logs to util

--- src/modules/telegram/scenarios/mealEvent/utils.ts
+++ src/modules/telegram/scenarios/mealEvent/utils.ts
@@ -1,18 +1,26 @@
+import { Logger } from '@nestjs/common'
 import { time } from 'src/helpers'
 
 export const getTimeInfoForNotifications = () => {
+  const logger = new Logger('getTimeInfoForNotifications')
   const currentDateInstance = time().tz('Europe/Moscow')
 
   const currentDate = currentDateInstance.format('MM/DD/YYYY')
 
-  const formattedStartNotificationTime = '10:00:00'
-  const formattedEndNotificationTime = '22:00:00'
-  const formattedCurrentForStartNotificationDatetime = `${currentDate} ${formattedStartNotificationTime}`
-  const formattedCurrentForEndNotificationDatetime = `${currentDate} ${formattedEndNotificationTime}`
+  const formattedCurrentForStartNotificationDatetime = currentDateInstance.hour(10).minute(0).second(0)
+  const formattedCurrentForEndNotificationDatetime = currentDateInstance.hour(22).minute(0).second(0)
 
   const isNeedToPushNotification =
-    time(formattedCurrentForStartNotificationDatetime).tz('Europe/Moscow').isBefore(currentDateInstance) &&
-    time(formattedCurrentForEndNotificationDatetime).tz('Europe/Moscow').isAfter(currentDateInstance)
+    time(formattedCurrentForStartNotificationDatetime).isBefore(currentDateInstance) &&
+    time(formattedCurrentForEndNotificationDatetime).isAfter(currentDateInstance)
+
+  logger.log('[Current Time]', {
+    before: time(formattedCurrentForStartNotificationDatetime).isBefore(currentDateInstance),
+    after: time(formattedCurrentForEndNotificationDatetime).isAfter(currentDateInstance),
+    beforeTime: time(formattedCurrentForStartNotificationDatetime).format('MM/DD/YYYY HH:mm:ss'),
+    afterTime: time(formattedCurrentForEndNotificationDatetime).format('MM/DD/YYYY HH:mm:ss'),
+    current: time(currentDateInstance).format('MM/DD/YYYY HH:mm:ss'),
+  })
 
   return { isNeedToPushNotification, currentDate, currentDateInstance }
 }
